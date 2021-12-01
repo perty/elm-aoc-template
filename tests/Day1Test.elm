1e47c0f10a8ad7513a383396c0ff305e8e575fa5
perty <per@artcomputer.se>
2021-12-01 10:06:19 +0100

    Day1, problem 1.

---
+++ tests/Day1Test.elm
@@ -0,0 +1,36 @@
+module Day1Test exposing (..)
+
+import Day1
+import Expect exposing (Expectation)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "Day1"
+        [ describe "Problem 1"
+            [ test "Parser" <|
+                \_ -> Day1.parse testInput |> Expect.equal testInputAsInts
+            , test "As given" <|
+                \_ -> Day1.solution1 testInput |> Expect.equal 7
+            ]
+        ]
+
+
+testInput =
+    """
+    199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263
+    """
+
+
+testInputAsInts =
+    [ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 ]
