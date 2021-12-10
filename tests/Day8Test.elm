d202c151f53c0fa147749ccd075f8fcf1654b828
perty <per@artcomputer.se>
2021-12-10 16:47:11 +0100

    Day 8 parser

---
+++ tests/Day8Test.elm
@@ -0,0 +1,57 @@
+module Day8Test exposing (..)
+
+import Day8
+import Expect exposing (Expectation)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "Day8"
+        [ describe "Problem 1"
+            [ test "Parser" <|
+                \_ -> Day8.parse testInput4Parse |> Expect.equal testInputParsed
+            , test "As given" <|
+                \_ -> Day8.solution1 testInput |> Expect.equal 26
+            , test "From puzzle input" <|
+                \_ -> Day8.solution1 Day8.puzzleInput |> Expect.equal -1
+            ]
+        , describe "Problem 2"
+            [ test "As given" <|
+                \_ -> Day8.solution2 testInput |> Expect.equal -1
+            , test "From puzzle input" <|
+                \_ -> Day8.solution2 Day8.puzzleInput |> Expect.equal -1
+            ]
+        ]
+
+
+testInput4Parse =
+    """   be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+          edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+
+   """
+
+
+testInput =
+    """
+   be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+   edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+   fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+   fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+   aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+   fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+   dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+   bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+   egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+   gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+"""
+
+
+testInputParsed =
+    [ { signalPatterns = [ "be", "cfbegad", "cbdgef", "fgaecd", "cgeb", "fdcge", "agebfd", "fecdb", "fabcd", "edb" ]
+      , digits = [ "fdgacbe", "cefdb", "cefbgd", "gcbe" ]
+      }
+    , { signalPatterns = [ "edbfga", "begcd", "cbg", "gc", "gcadebf", "fbgde", "acbgfd", "abcde", "gfcbed", "gfec" ]
+      , digits = [ "fcgedb", "cgb", "dgebacf", "gc" ]
+      }
+    ]
