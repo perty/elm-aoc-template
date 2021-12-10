035bcd7a701965d7e51055c883d178f24ac62f5c
perty <per@artcomputer.se>
2021-12-10 08:42:00 +0100

    Day 7 problem 1 solved

---
+++ tests/Day7Test.elm
@@ -0,0 +1,33 @@
+module Day7Test exposing (..)
+
+import Day7
+import Expect exposing (Expectation)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "Day7"
+        [ describe "Problem 1"
+            [ test "Parser" <|
+                \_ -> Day7.parse testInput |> Expect.equal testInputAsInts
+            , test "As given" <|
+                \_ -> Day7.solution1 testInput |> Expect.equal 37
+            , test "From puzzle input" <|
+                \_ -> Day7.solution1 Day7.puzzleInput |> Expect.equal 352997
+            ]
+        , describe "Problem 2"
+            [ test "As given" <|
+                \_ -> Day7.solution2 testInput |> Expect.equal -1
+            ]
+        ]
+
+
+testInput =
+    """
+16,1,2,0,4,2,7,1,2,14
+    """
+
+
+testInputAsInts =
+    [ 16, 1, 2, 0, 4, 2, 7, 1, 2, 14 ]
